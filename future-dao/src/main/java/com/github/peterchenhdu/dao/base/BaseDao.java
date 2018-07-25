0f3042fd244f53be7254a017f3a903d2b11d1cf0
cp_hdu@163.com <cp_hdu@163.com>
2018-07-25 21:11:00 +0800

    目录结构提交

---
+++ future-dao/src/main/java/com/github/peterchenhdu/dao/base/BaseDao.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011-2025 PiChen
+ */
+
+package com.github.peterchenhdu.dao.base;
+
+/**
+ * Class Description...
+ *
+ * @author chenpi
+ * @since 2018/7/25 21:02
+ */
+public class BaseDao {
+}
---
+++ future-service/src/main/java/com/github/peterchenhdu/service/base/BaseController.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011-2025 PiChen
+ */
+
+package com.github.peterchenhdu.service.base;
+
+/**
+ * Class Description...
+ *
+ * @author chenpi
+ * @since 2018/7/25 21:04
+ */
+public class BaseController {
+}
