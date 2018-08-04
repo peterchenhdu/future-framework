e950432eb9532a6688e508576ab7f4fefe7374c2
cp_hdu@163.com <cp_hdu@163.com>
2018-08-04 23:22:55 +0800

    mb...

---
+++ future-tool/future-tool-mg/src/main/java/com/github/peterchenhdu/future/tool/mb/App.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2011-2025 PiChen
+ */
+
+package com.github.peterchenhdu.future.tool.mb;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.mybatis.generator.internal.NullProgressCallback;
+import org.springframework.util.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chenpi
+ * @since 2018/8/3 22:57
+ */
+public class App {
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<>();
+        //如果这里出现空指针，直接写绝对路径即可。
+        String genCfg = "/generatorConfig.xml";
+        File configFile = new File(App.class.getResource(genCfg).getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException | XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            Assert.notNull(config, "config is null");
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            Assert.notNull(myBatisGenerator, "myBatisGenerator is null");
+            myBatisGenerator.generate(new NullProgressCallback());
+        } catch (SQLException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        warnings.forEach(System.err::println);
+    }
+}
