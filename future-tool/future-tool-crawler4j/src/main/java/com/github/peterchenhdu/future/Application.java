86cc48b36b01c2b01c44ad3a05233918ff99072c
cp_hdu@163.com <cp_hdu@163.com>
2018-08-10 23:50:22 +0800

    update...

--- future-tool/future-tool-crawler4j/src/main/java/com/github/peterchenhdu/future/Application.java
+++ future-tool/future-tool-crawler4j/src/main/java/com/github/peterchenhdu/future/Application.java
@@ -17,7 +17,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @since 2018/8/3 22:57
  */
 @SpringBootApplication
-@MapperScan(basePackages = "com.github.peterchenhdu.future.tool.mb.crawler4j.crawler4j.dao")
+@MapperScan(basePackages = "com.github.peterchenhdu.future.tool.crawler4j.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
