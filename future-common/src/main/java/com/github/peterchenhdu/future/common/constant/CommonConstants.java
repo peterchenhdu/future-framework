7de06906bed8ca78be74bc79195116874c43ef25
cp_hdu@163.com <cp_hdu@163.com>
2018-08-18 20:30:30 +0800

    update...

---
+++ future-common/src/main/java/com/github/peterchenhdu/future/common/constant/CommonConstants.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2011-2025 PiChen
+ */
+
+package com.github.peterchenhdu.future.common.constant;
+
+/**
+ * @author PiChen
+ * @since 2018/8/18 18:38
+ */
+public class CommonConstants {
+    public static final String BLANK_STR = "";
+}
